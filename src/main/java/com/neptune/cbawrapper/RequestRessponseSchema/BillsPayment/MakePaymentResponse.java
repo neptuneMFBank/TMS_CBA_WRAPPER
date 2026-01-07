1c6e87a918cab38cd15aacb169df8d2ff70290e0
abel <abelkelly6022@gmail.com>
2026-01-07 17:17:46 +0100

    updated name enquiry service

--- src/main/java/com/neptune/cbawrapper/RequestRessponseSchema/BillsPayment/MakePaymentResponse.java
+++ src/main/java/com/neptune/cbawrapper/RequestRessponseSchema/BillsPayment/MakePaymentResponse.java
@@ -1,5 +1,8 @@
 package com.neptune.cbawrapper.RequestRessponseSchema.BillsPayment;
 
+import lombok.ToString;
+
+@ToString
 public class MakePaymentResponse {
     private boolean success;
     private String message;
