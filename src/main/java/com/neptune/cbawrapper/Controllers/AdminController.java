626e246ff3b4fea15db7fd7e85f60c0b6fb21cbc
abel <abelkelly6022@gmail.com>
2026-09-24 16:12:06 +0100

    updated logs

--- src/main/java/com/neptune/cbawrapper/Controllers/AdminController.java
+++ src/main/java/com/neptune/cbawrapper/Controllers/AdminController.java
@@ -198,10 +198,10 @@ public class AdminController {
                     virtualAccountModel.get().getPhone_number()
             );
 
-            if (!phoneNumber.startsWith("+234")) {
+            if (!phoneNumber.startsWith("234")) {
                 phoneNumber = phoneNumber.startsWith("0")
-                        ? "+234" + phoneNumber.substring(1)
-                        : "+234" + phoneNumber;
+                        ? "234" + phoneNumber.substring(1)
+                        : "234" + phoneNumber;
             }
             log.info("phoneNumber {} ", phoneNumber);
             String message = "Your OTP to reset your Admin PIN is " + token + ". It expires in " + adminPinResetOtpExpiryMinutes + " minutes. Do not share this code with anyone.";
